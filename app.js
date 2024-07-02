3ddbe5637ba697eaa93c0ae5d2bb6ad73aefbb10
Bianca Zatorre <biancazatorre@hotmail.com>
2024-07-02 14:26:34 -0300

    Update JS

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 alert ('Sorteie seu número');
 let numeroSecreto = 6;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10')
+let chute
 
-if (chute == 6){
-    alert(`Você descobriu' ${numeroSecreto}`);
-}
-else{
-    if(chute > numeroSecreto){
-        alert(`O número é menor que ${chute}`);
+while(chute != numeroSecreto ){
+    chute = prompt ('Ecolha um número entre 1 e 10');
+
+    if (chute == numeroSecreto){
+        alert(`Você descobriu, é ${numeroSecreto}`);
     }
     else{
-        alert(`O número é maior que ${chute}`);
+        if(chute > numeroSecreto){
+            alert(`O número é menor que ${chute}`);
+        }
+        else{
+            alert(`O número é maior que ${chute}`);
+        }
     }
 }
+
+
