d913b821cc5ffb9d5d9384cbc4fbb5427e3771fb
Bianca Zatorre <biancazatorre@hotmail.com>
2024-07-04 14:40:54 -0300

    querySelector

--- js-curso-2-aula_-/app2.js
+++ js-curso-2-aula_-/app2.js
@@ -1,2 +1,5 @@
 let titulo = document.querySelector('h1');
-titulo.innerhtml = 'Jogo do número secreto';
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML ='Escolha um número entre 1 e 10'
