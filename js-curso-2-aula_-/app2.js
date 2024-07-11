be1d58f79aad365de89086b8881e3ee037f309ac
Bianca Zatorre <biancazatorre@hotmail.com>
2024-07-11 14:56:41 -0300

    limparcampo

--- js-curso-2-aula_-/app2.js
+++ js-curso-2-aula_-/app2.js
@@ -1,7 +1,8 @@
 let numeroSecreto= gerarNumeroAleatorio();
+let tentativas = 1;
 
-function exibirTextoNaTela(){
-    let campo = document.querySelector(tag)
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
@@ -9,9 +10,31 @@ exibirTextoNaTela('h1', 'Jogo do número Secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log('O botão foi clicado');
+    let chute = document.querySelector ('input').value;
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : tentativas;
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }
+        else{
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas ++;
+        limparCampo();
+    }
+}
+//função pra gerar numero aleatorio
+//parseint pra ser inteiro
+//
+function gerarNumeroAleatorio (){
+    return parseInt(Math.random() * 10 + 1)
 }
 
-function gerarNumeroAleatorio (){
-    parseInt (Math.random() * 10 + 1)
+function limparCampo (){
+    chute = document.queryselector ('input');
+    chute.value ='';
 }
